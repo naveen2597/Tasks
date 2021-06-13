9dba3063a2246cdeb844183c388d3c669364fd28
naveen2597 <84046325+naveen2597@users.noreply.github.com>
2021-06-14 00:54:50 +0530

    Add files via upload

---
+++ Day-12_Project/src/app/base/user-table/user-table.component.ts
@@ -0,0 +1,30 @@
+
+import { Component, OnInit } from '@angular/core';
+import {UserService} from 'src/app/user.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.css']
+})
+export class UserTableComponent implements OnInit {
+ 
+  constructor(private tuser:UserService,private back:Router,private location:Location) { }
+ loc:any=this.tuser.table1;
+ detail:any;
+  ngOnInit(): void {
+  }
+  goBack() {
+    this.back.navigateByUrl('login');
+  }
+  editInfo() {
+    this.detail = this.loc;
+  }
+  updInfo() {
+    this.location.back();
+    alert("Successfully Updated...");
+    console.log(this.detail.users);
+  }
+
+}
